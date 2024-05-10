215942575d9b6008bf17d9821428250534d1f145
Taylor Noble <zachnoble1993@gmail.com>
2024-05-10 10:40:49 -0400

    Add project files.

---
+++ MathGame/Game.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Game
+    {
+        Random rand;
+        public int Rounds { get; private set; }
+        public Level Difficulty { get; private set; }
+        public Type Operation { get; private set; }
+        public int Results { get; private set; }
+        public enum Level
+        {
+            Easy = 1, // max value 10**1
+            Medium, // max value 10**2
+            Hard // max value 10**3
+        };
+
+        public enum Type
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division,
+            Random
+        }
+
+
+        public Game(int rounds, Level difficulty, Type operation)
+        {
+            rand = new Random();
+            this.Rounds = rounds;
+            this.Difficulty = difficulty;
+            this.Operation = operation;
+        }
+
+        private void gamePrompt()
+        {
+            Console.WriteLine("Welcome to the Math Quiz game.");
+            if (Rounds > 1)
+            {
+                Console.WriteLine($"You will be given {Rounds} problems to solve.");
+            } 
+            else
+            {
+                Console.WriteLine($"You will be given {Rounds} problem to solve.");
+            }
+            Console.WriteLine("All answers will be integers (no decimals answers).");
+        }
+
+        private List<int> getProblem()
+        {
+            // Randomly generate math problem
+            List<int> prob = new List<int>();
+
+
+            int op;
+            if (Operation == Type.Random)
+            {
+                op = rand.Next(0, 4);
+            }
+            else
+            {
+                op = (int)Operation;
+            }
+
+            prob.Add(op);
+            int max = (int) Math.Pow(10.0, (double)Difficulty);
+            int a = rand.Next(0, 11);
+            int b = rand.Next(1, 11);
+            prob.Add(a);
+
+            if (op == 0)
+            {
+                prob.Add(b);
+                prob.Add(a + b);
+            }
+            else if (op == 1)
+            {
+                prob.Add(b);
+                prob.Add(a - b);
+
+            }
+            else if (op == 2)
+            {
+                prob.Add(b);
+                prob.Add(a * b);
+
+            }
+            else if (op == 3)
+            {
+                while (a % b != 0)
+                {
+                    b = rand.Next(1, 11);
+                }
+                prob.Add(b);
+                prob.Add(a / b);
+            }
+
+
+            return prob;
+        }
+        
+        public void Run()
+        {
+            gamePrompt();
+            for (int i = 0; i < Rounds; i++)
+            {
+                List<int> prob = getProblem();
+
+                String op = prob[0] switch
+                {
+                    0 => "+",
+                    1 => "-",
+                    2 => "*",
+                    3 => "/",
+                    _ => "?"
+                };
+
+                Console.Write($"{prob[1]} {op} {prob[2]} = ");
+
+                try
+                {
+                    String? input = Console.ReadLine();
+                    if (input != null) 
+                    { 
+                        if (input == "q")
+                            break;
+
+                        int user_answer = int.Parse(input);
+                        if (user_answer == prob[3])
+                        {
+                            Console.WriteLine("\n\tCorrect!");
+                            Results++;
+                        } else
+                        {
+                            Console.WriteLine($"\n\tIncorrect! The answer is {prob[3]}");
+                        }
+                    }
+                }
+                catch (Exception ex) when (ex is ArgumentNullException || ex is FormatException || ex is OverflowException)
+                {
+                    Console.WriteLine("\nError in reading input\n Exiting game.");
+                    break;
+                }
+            }
+        }
+
+        public String ResultsToString()
+        {
+            if (Rounds > 1)
+            {
+                return $"Won {Results} out of {Rounds} rounds";
+            }
+            else
+            {
+                return $"Won {Results} out of {Rounds} round.";
+            }
+        }
+
+    }
+}
---
+++ MathGame/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace MathGame
+{
+    internal class Program
+    {
+        /** 
+         * Math Quiz game 
+         */
+        static void Main(string[] args)
+        {
+            List<Game> games = new List<Game>();
+
+            while (true)
+            {
+                int? diffInput = null;
+                int? typeInput = null;
+                int? rounds = null;
+                Console.WriteLine("Enter the game Settings (q to Quit): ");
+                while (diffInput == null)
+                {
+                    diffInput = getDifficulty();
+                    if (diffInput == -1) return;
+                }
+                while (typeInput == null)
+                {
+
+                    typeInput = getType();
+                    if (typeInput == -1) return;
+                }
+                while (rounds == null)
+                {
+                    rounds = getRounds();
+                    if (rounds == -1) return;
+                }
+
+                Game game = new Game( (10 + (rounds.Value * 5)), (Game.Level)diffInput, (Game.Type)typeInput);
+                game.Run();
+                games.Add(game);
+                Console.WriteLine(game.ResultsToString());
+                Console.WriteLine("Would you like to play again? ('p' to see previous games history");
+                int? input = getInput(['y', 'n', 'p']);
+                if (input != null && input.Value == -3) {
+                    break;
+                } 
+                if (input != null && input.Value == -4)
+                {
+                    Console.WriteLine("Previous Games:");
+                    foreach (Game g in games)
+                    {
+                        Console.WriteLine(g.ResultsToString());
+                    }
+                }
+            }
+
+        }
+
+
+        static int? getInput(List<char> validInputs)
+        {
+            int? input = null;
+            String? inputRaw = Console.ReadLine();
+            if (inputRaw != null && inputRaw.Length == 1)
+            {
+                char inputChar = (char)inputRaw[0];
+                if (validInputs.Contains(inputChar))
+                {
+                    if (inputChar == 'q')
+                        return -1;
+                    if (Char.ToLower(inputChar) == 'y')
+                        return -2;
+                    if (Char.ToLower(inputChar) == 'n')
+                        return -3;
+                    if (char.ToLower(inputChar) == 'p')
+                        return -4;
+
+                    if (Char.IsNumber(inputChar))
+                        input = int.Parse(inputChar.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid option.");
+                    return null;
+                }
+            }
+
+            return input;
+
+        }
+        static int? getDifficulty()
+        {
+
+            Console.WriteLine("Difficulty:");
+            Console.WriteLine("\t1. Easy: Max value of operand is 10.");
+            Console.WriteLine("\t2. Medium: Max value of operand is 100.");
+            Console.WriteLine("\t3. Hard: Max value of operand is 1000.");
+            return getInput(['1', '2', '3', 'q']);
+
+        }
+
+        static int? getType()
+        {
+            Console.WriteLine("Type:");
+            Console.WriteLine("\t0. Addition");
+            Console.WriteLine("\t1. Subtraction");
+            Console.WriteLine("\t2. Muliplication");
+            Console.WriteLine("\t3. Division");
+            Console.WriteLine("\t4. Random");
+            return getInput(['0', '1', '2', '3', '4', 'q']);
+        }
+
+        static int? getRounds()
+        {
+            Console.WriteLine("Rounds: ");
+            Console.WriteLine("\t0. 10 Rounds");
+            Console.WriteLine("\t1. 15 Rounds");
+            Console.WriteLine("\t2. 20 Rounds");
+            Console.WriteLine("\t3. 25 Rounds");
+            return getInput(['0', '1', '2', '3', 'q']);
+        }
+
+    }
+}
